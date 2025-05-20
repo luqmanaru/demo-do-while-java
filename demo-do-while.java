5102b7804a3fc41f916e9e1045f3e30aa7304d28
Hanif Luqmanul Hakim <hanifluqmanulhakim20@gmail.com>
2025-05-20 14:21:24 +0700

    Create demo-do-while.java

---
+++ demo-do-while.java
@@ -0,0 +1,15 @@
+/**
+ * Demonstrasi Do-While Loop
+ * @author luqmanaru
+ */
+public class PerulanganDoWhile {
+    public static void main(String[] args) { 
+        int x = 1; // Inisialisasi counter
+        
+        // Do-while loop
+        do {
+            System.out.println(x); 
+            x++; // Increment counter
+        } while (x < 10);
+    }
+}
